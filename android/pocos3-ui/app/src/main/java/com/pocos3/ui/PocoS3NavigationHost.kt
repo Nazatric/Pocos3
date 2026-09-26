94db68dc0763dc172812fcc0824f92195fa7b85b
Nazatric <nazakat1930@gmail.com>
2026-09-26 21:29:57 +0530

    Add missing remember import to PocoS3NavigationHost

--- android/pocos3-ui/app/src/main/java/com/pocos3/ui/PocoS3NavigationHost.kt
+++ android/pocos3-ui/app/src/main/java/com/pocos3/ui/PocoS3NavigationHost.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
